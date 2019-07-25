361086aaf8ab8b6035797bc8dafd21140b372e47
Karissa <karissa.whiting@gmail.com>
2019-07-25 12:56:53 -0400

    tryin to get label function to exportttttt

---
+++ R/names_to_labels.R
@@ -0,0 +1,28 @@
+
+#' Convert DF names to labels
+#'
+#' @param df a data frame
+#' @param caps a string of capitalization exceptions
+#' @export
+#'
+#'
+# labelling funs-------------------------------------------------------------
+
+# function to transform names of a dataframe to friendly labels
+names_to_labs <- function(df, caps = NULL) {
+  names <- names(df)
+
+  if (!is.null(caps)) {
+    named_vec <- c(toupper(caps))
+    names(named_vec) <- tolower(caps)
+  }
+
+  labels <- string::str_replace_all(names, "_", " ") %>%
+    tolower(.) %>%
+    tools::toTitleCase(.) %>%
+    purrr::when(!is.null(caps), ~stringr::str_replace_all(tolower(.), named_vec))
+
+  return(labels)
+}
+
+
